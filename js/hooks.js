64c28579ec8c5353ad5132aab4428129f64ad301
Mark Manguno <mmanguno@liveperson.com>
2020-03-16 15:54:03 -0400

    hooks

---
+++ js/hooks.js
@@ -0,0 +1,30 @@
+const hooks = [
+    'AFTER_GET_SURVEY',
+    'BEFORE_SUBMIT_SURVEY',
+    'BEFORE_SEND_VISITOR_LINE',
+    'AFTER_GET_SURVEY',
+    'BEFORE_SEND_SDE'
+];
+
+function log (body) {
+    let _body = typeof body === 'string' ?
+      body : JSON.stringify(body);
+    console.log(_body);
+}
+
+function init () {
+    hooks.map(hook => {
+        lpTag.hooks.push({
+            name: hook,
+            callback: log
+        })
+    })
+}
+
+if (window.lpTag) init();
+else let hooksInterval = window.setInterval(() => {
+    if (window.lpTag) {
+        window.clearInterval(hooksInterval);
+        init();
+    }
+});
