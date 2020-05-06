1817f9dd4742bfae1b73461aa2900de624a5ab2c
Mark Manguno <mmanguno@liveperson.com>
2020-05-06 15:12:45 -0400

    auth with identities

--- js/util/auth.js
+++ js/util/auth.js
@@ -1,7 +1,23 @@
 window.params = window.params || new URLSearchParams(window.location.search);
+window.lpTag = window.lpTag || {};
+lpTag.identities = lpTag.identities || [];
+
+let identity = null;
 
 if (params.has('sub')) {
-    lpTag.sdes.push({'type': 'ctmrinfo', 'info': { customerId : params.get('sub') }});
+    identity = {
+        iss: document.location.host,
+        acr: 'loa1',
+        sub: params.get('sub')
+    }
 } else if (params.has('randomsub')) {
-    lpTag.sdes.push({'type': 'ctmrinfo', 'info': { customerId : Math.random().toString(36).substring(2) }});
+    identity = {
+        iss: document.location.host,
+        acr: 'loa1',
+        sub: Math.random().toString(36).substring(2)
+    }
 }
+
+lpTag.identities.push(function (cb) { cb(identity) })
+
+
