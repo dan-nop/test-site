1d4bf93d0e2d60cd8f73ef75b3e7070c621cf528
Mark Manguno <mmanguno@liveperson.com>
2020-05-07 18:38:46 -0400

    agent initiated widget 1.0

--- js/scripts/agentinitengwidget.js
+++ js/scripts/agentinitengwidget.js
@@ -1,54 +1,61 @@
-//Add each language's widget toggle phrase to this array. It will be used to check to see if the widget needs to toggle.
-//This is here due to the fact that this script may not have any access to a specific language variable or information.
-//This will never be triggered by the events caused when reloading a page or navigating.
-//This will only trigger once off of a single hook call.
-
 lpTag = window.lpTag || {};
+// putting this stuff in lpTag.external.agentInitWidget so as not to pollute the global namespace
 lpTag.external = lpTag.external || {};
 lpTag.external.agentInitWidget = {
-    activationRegex: /Widget Activate: (https:\/\/.+)/g,
+    // here you will define the regular expression that indicates that a widget link has been sent
+    activationRegex: /Widget Activate: (https:\/\/.+)/,
+    // this makes sure that the first burst of messages on a page load doesn't trigger the widget based on prior qualifying agent messages
     firstMessage: true,
+    // function to determine whether the widget is already displayed
     isWidgetDisplayed: function () { return document.querySelector(".lp_slider_open") != null },
+    // function to process incoming conversation lines
     processLines: function (lineData) {
-        // console.log(lineData);
+        // console.log("firstMessage", lpTag.external.agentInitWidget.firstMessage)
+        // console.log("lineData", lineData);
+        var lines = lineData && lineData.data && lineData.data.lines;
+        if (!lines) return false;
+        // console.log("lines", lines)
+
+        // identify whether the widget is available on this window
+        var toggleElement = document.querySelector("button[data-lp-point='widget_sdk']");
+        if (!toggleElement) return false;
+        // console.log("toggleElement", toggleElement)
 
         // is this data from the current conversation?
         var currentConversation = true;
-        lineData.data.lines.forEach(function (line) {
-            if (line.history) currentConversation = false;
+        for (var i = 0, l = lines.length; i < l; i++) {
+            if (lines[i].history) {
+                currentConversation = false;
+                break;
+            }
+        }
+        // console.log("currentConversation", currentConversation);
+
+        // cycle through lines, hide those that match, and if this is a new agent message from the current conversation pop the widget and populate the src url
+        lines.forEach(function (line) {
+            if (line.source === "agent") {
+                let match = lpTag.external.agentInitWidget.activationRegex.exec(line.text);
+                // console.log("match", match);
+                if (match) {
+                    if (currentConversation && !lpTag.external.agentInitWidget.firstMessage) {
+                        if (!lpTag.external.agentInitWidget.isWidgetDisplayed()) toggleElement.click()
+                        var iframeElement = document.querySelector('div#LP_WidgetViewController_1>iframe');
+                        iframeElement.src = match[1]
+                    }
+                    delete line.text;
+                }
+            }
         })
 
-        var toggleElement = document.querySelector("button[data-lp-point='widget_sdk']");
-        if (toggleElement) {
-            // console.log("first message", lpTag.external.agentInitWidget.firstMessage);
-
-            lineData.data.lines.forEach(function (line) {
-                if (line.source === "agent") {
-                    let match = lpTag.external.agentInitWidget.activationRegex.exec(line.text);
-                    // console.log(match);
-                    if (match) {
-                        // open the widget if this line is from the current conversation and this is not the initial
-                        // burst of messages from a page refresh
-                        if (currentConversation && !lpTag.external.agentInitWidget.firstMessage) {
-                            if (!lpTag.external.agentInitWidget.isWidgetDisplayed()) toggleElement.click()
-                            var iframeElement = document.querySelector('div#LP_WidgetViewController_1>iframe');
-                            iframeElement.src = match[1]
-                        }
-                        // if this was the first burst of messages set firstMessage to false so the next will be processed
-                        if (currentConversation && lpTag.external.agentInitWidget.firstMessage) {
-                            lpTag.external.agentInitWidget.firstMessage = false;
-                        }
-                        // remove the line from the visitor's view
-                        delete line.text;
-                    }
-                }
-            });
-
+        // if this was the first set of messages from the initial page load set that flag to false now
+        if (lpTag.external.agentInitWidget.firstMessage) {
+            lpTag.external.agentInitWidget.firstMessage = false;
         }
-
+        // console.log("firstMessage", lpTag.external.agentInitWidget.firstMessage)
     }
 }
 
+// attach the hook
 lpTag.hooks = lpTag.hooks || [];
 lpTag.hooks.push({
     name: "AFTER_GET_LINES",
