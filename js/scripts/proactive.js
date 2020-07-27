7138cf572ac2c4af8be6b3ff5cfc4969d253b328
Mark Manguno <mmanguno@liveperson.com>
2020-07-27 15:55:42 -0400

    proactive.js

--- js/scripts/proactive.js
+++ js/scripts/proactive.js
@@ -3,6 +3,7 @@ waitForTag(proactivePopInit);
 function proactivePopInit () {
     lpTag.external = lpTag.external || {};
     lpTag.external.flags = lpTag.external.flags || {};
+    lpTag.external.flags.eligibleForAutoClick = determineAutoClickEligibility();
     lpTag.events.bind({
         eventName: 'AFTER_CREATE_ENGAGEMENT_INSTANCE',
         appName: 'RENDERER_STUB',
@@ -10,19 +11,23 @@ function proactivePopInit () {
     });
 }
 
+function determineAutoClickEligibility() {}
+
 function autoClickProactive (data) {
-    console.groupCollapsed(`engagement displayed`)
-        console.log(`proactive ${data.eng.engData.engagementName}`)
+    console.groupCollapsed(`engagement ${data.eng.engData.engagementName} displayed`)
         console.log(`engagmentId ${data.eng.engData.engagementId}`)
         console.log(`skill ${data.conf.skillName}`)
         console.dir(data)
     console.groupEnd()
+    // is this a proactive engagement?
+    if (!(data && data.eng && data.eng.engData && data.eng.engData.engagementType === 1)) return false;
 
-    // is engagement proactive (engagementType = 1) and has this visitor already been invited on this page
-    if (data && data.eng && data.eng.engData && data.eng.engData.engagementType === 1 && !lpTag.external.flags.invited) {
-        window.setTimeout(() => {
-            lpTag.taglets.rendererStub.click(data.eng.engData.engagementId)
-            lpTag.external.flags.invited = true;
-        }, 100);
-    }
+    // has this person already dismissed an invitation in this section in the past 24 hours?
+    // recommend setting a 24 hour cookie when auto-clicking the engagement, then checking for that cookie here and not popping the engagement if present
+
+    // is there availability in the relevant skill?
+    window.setTimeout(() => {
+        lpTag.taglets.rendererStub.click(data.eng.engData.engagementId)
+        lpTag.external.flags.eligibleForAutoClick = false;
+    }, 100);
 }
