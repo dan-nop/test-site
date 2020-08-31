fcbe6f2cfb35d60223b4d4fa49e81a2aeaca9445
Mark Manguno <mmanguno@liveperson.com>
2020-08-31 15:09:06 -0400

    error sde

--- js/util/sdes.js
+++ js/util/sdes.js
@@ -16,6 +16,24 @@ function pushSDE(type) {
                 payload.service.serviceId = document.getElementById('serviceActivityServiceId').value
             }
             break;
+        case 'error':
+            payload.error = {}
+            if (document.getElementById('errorContextId').value) {
+                payload.error.contextId = document.getElementById('errorContextId').value
+            }
+            if (document.getElementById('errorMessage').value) {
+                payload.error.message = document.getElementById('errorMessage').value
+            }
+            if (document.getElementById('errorCode').value) {
+                payload.error.code = document.getElementById('errorCode').value
+            }
+            if (document.getElementById('errorLevel').value !== 'Undefined') {
+                payload.error.level = parseInt(document.getElementById('errorLevel').value)
+            }
+            if (document.getElementById('errorResolved').value) {
+                payload.error.resolved = document.getElementById('errorResolved').checked
+            }
+            break;
     }
     console.log(`push payload: ${JSON.stringify(payload)}`)
     lpTag.sdes.push(payload);
