2d157252a4c7d3ab6a5e6579eb0f6cad585a6f31
Mark Manguno <mmanguno@liveperson.com>
2020-11-02 12:07:54 -0500

    waitfortag named intervals

--- js/util/waitfortag.js
+++ js/util/waitfortag.js
@@ -1,10 +1,11 @@
 function waitForTag (callback) {
     if (window.lpTag.loaded) callback();
     else {
+        window._intervals = window._intervals || {};
         console.log(`delaying execution of ${callback.name} ${lpTag._timing && lpTag._timing.start ? ' :: '+(new Date().getTime() - lpTag._timing.start) : ''}`)
-        let _interval = window.setInterval(() => {
-            if (window.lpTag && window.lpTag.loaded) {
-                window.clearInterval(_interval);
+        window._intervals[callback.name] = window.setInterval(() => {
+            if (window.lpTag && window.lpTag.loaded) { 
+                window.clearInterval(window._intervals[callback.name]);
                 callback();
             } else {
                 console.log(`delaying execution of ${callback.name} ${lpTag._timing && lpTag._timing.start ? ' :: '+(new Date().getTime() - lpTag._timing.start) : ''}`)
