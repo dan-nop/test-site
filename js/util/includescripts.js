ea474fcfc6e98c3edc5bab7af4f3ead7a3b0732d
Mark Manguno <mmanguno@liveperson.com>
2021-03-11 17:34:18 -0500

    load styles

--- js/util/includescripts.js
+++ js/util/includescripts.js
@@ -1,5 +1,6 @@
 window.params = window.params || new URLSearchParams(window.location.search);
 let scripts = [];
+let styles = [];
 
 if (params.get('scripts')) {
     scripts = params.get('scripts').split(',')
@@ -8,5 +9,17 @@ scripts.forEach(script => {
     let s = document.createElement('script');
     s.type = 'text/javascript';
     s.src = 'js/scripts/' + script + '.js';
+    document.head.appendChild(s);
+});
+
+if (params.get('styles')) {
+    styles = params.get('styles').split(',')
+}
+
+styles.forEach(style => {
+    let s = document.createElement('link');
+    s.type = 'text/css';
+    s.rel = 'stylesheet';
+    s.href = 'css/styles/' + style + '.css';
     document.head.appendChild(s);
 });
