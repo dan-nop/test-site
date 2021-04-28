3eee90b47839d4a43f2dabd62d827d0dfc8192d2
Mark Manguno <mmanguno@liveperson.com>
2021-04-28 18:02:10 -0400

    accessibilityfix.js 3

--- js/external/accessibilityfix.js
+++ js/external/accessibilityfix.js
@@ -1,54 +1,32 @@
-// Parse through script url params and associated actions
-function getScriptParams() {
-    let params = {}
-    for (i = 0; i < document.scripts.length; i++) {
-        var s = document.scripts[i].src;
-        if (s.indexOf("accessibilityfix.js?") > -1) {
-            var p = s.split("?")[1].split("&");
-            for (n = 0; n < p.length; n++) {
-                key = p[n].split("=")[0];
-                val = p[n].split("=")[1];
-                switch (key) {
-                    case "divids":
-                        // Get div ids
-                        console.log('divids: ', val)
-                        params.divids = val.split(',')
+lpTag.external = lpTag.external || {};
+lpTag.external.accessibilityFix = {
+    getZones: function () {
+        var zonesLoaded = lpTag.events.hasFired("SCRAPER","ZONES_LOADED");
+        if (zonesLoaded && zonesLoaded[0] && zonesLoaded[0].data) {
+            this.zones = zonesLoaded[0].data;
+        }
+        return this.zones;
+    },
+    engagementRenderedHandler: function (data, b, c) {
+        console.log(data,b,c)
+        try {
+            var possibleZones = this.zones || this.getZones();
+            if (data.data.engagementType === 5) {
+                var offerZone = data.data.zoneId;
+                var thisZone = possibleZones.find(function (possibleZone) { return possibleZone.id === offerZone})
+                // remove role and tabindex from relevant div
+                if (thisZone) {
+                    const selectedDiv = document.getElementById(thisZone.name);
+                    const selectedDivLpm = selectedDiv.querySelector('.LPMcontainer');
+                    if (selectedDivLpm.getAttribute('role') === 'button') selectedDivLpm.removeAttribute('role');
+                    if (selectedDivLpm.getAttribute('tabindex') === '0') selectedDivLpm.removeAttribute('tabindex');
                 }
             }
+        } catch (e) {
+            console.error();
         }
     }
-    return params;
 }
 
-function getDivs () {
-    var divs = [];
-    var zonesLoaded = lpTag.events.hasFired("SCRAPER","ZONES_LOADED")
-    if (zonesLoaded && zonesLoaded[0] && zonesLoaded[0].data) {
-        zonesLoaded[0].data.forEach(function (zone) {
-            if (zone.zoneType === 0) {
-                divs.push(zone.name)
-            }
-        })
-    }
-    return divs;
-}
-
-
-
-function engagementRenderedHandler () {
-    try {
-        const divs = getDivs();
-
-        // remove role and tabindex from specified divs above
-        divs.forEach((divName) => {
-            const selectedDiv = document.getElementById(divName);
-            const selectedDivLpm = selectedDiv.querySelector('.LPMcontainer');
-            if (selectedDivLpm.getAttribute('role') === 'button') selectedDivLpm.removeAttribute('role');
-            if (selectedDivLpm.getAttribute('tabindex') === '0') selectedDivLpm.removeAttribute('tabindex');
-        });
-    } catch (e) {
-        console.error();
-    }
-}
-
-engagementRenderedHandler();
+lpTag.events.bind('LP_OFFERS','OFFER_IMPRESSION', lpTag.external.engagementRenderedHandler);
+console.log('accessibilityfix loaded');
