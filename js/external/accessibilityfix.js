787291151be052e4bcb475bd8c1eef79fa415f2e
Mark Manguno <mmanguno@liveperson.com>
2021-04-28 17:14:47 -0400

    accessibilityfix.js 2

--- js/external/accessibilityfix.js
+++ js/external/accessibilityfix.js
@@ -1,5 +1,6 @@
 // Parse through script url params and associated actions
 function getScriptParams() {
+    let params = {}
     for (i = 0; i < document.scripts.length; i++) {
         var s = document.scripts[i].src;
         if (s.indexOf("accessibilityfix.js?") > -1) {
@@ -9,10 +10,30 @@ function getScriptParams() {
                 val = p[n].split("=")[1];
                 switch (key) {
                     case "divids":
-                        // Get div id to append
+                        // Get div ids
                         console.log('divids: ', val)
+                        params.divids = val.split(',')
                 }
             }
         }
     }
+    return params;
 }
+
+function engagementRenderedHandler () {
+    try {
+        const divs = getScriptParams().divids;
+
+        // remove role and tabindex from specified divs above
+        divs.forEach((divName) => {
+            const selectedDiv = document.getElementById(divName);
+            const selectedDivLpm = selectedDiv.querySelector('.LPMcontainer');
+            if (selectedDivLpm.getAttribute('role') === 'button') selectedDivLpm.removeAttribute('role');
+            if (selectedDivLpm.getAttribute('tabindex') === '0') selectedDivLpm.removeAttribute('tabindex');
+        });
+    } catch (e) {
+        console.error();
+    }
+}
+
+engagementRenderedHandler();
