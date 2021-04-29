d3f0ed528278feba3a20422eda598ec5922c55e0
Mark Manguno <mmanguno@liveperson.com>
2021-04-29 10:21:20 -0400

    accessibilityfix.js 10

--- js/external/accessibilityfix.js
+++ js/external/accessibilityfix.js
@@ -1,23 +1,19 @@
 // put custom code in lpTag.external to keep the js namespace clean
 lpTag.external = lpTag.external || {};
 lpTag.external.accessibilityFix = {
-    // get and cache all of the zones on the account
-    getZones: function () {
-        var zonesLoaded = lpTag.events.hasFired("SCRAPER","ZONES_LOADED");
-        if (zonesLoaded && zonesLoaded[0] && zonesLoaded[0].data) {
-            lpTag.external.accessibilityFix.zones = zonesLoaded[0].data;
-        }
-        return lpTag.external.accessibilityFix.zones;
-    },
     // handle the offer_impression event
     engagementRenderedHandler: function (data) {
         try {
             // fetch zones
-            var possibleZones = lpTag.external.accessibilityFix.zones || lpTag.external.accessibilityFix.getZones();
+            var zonesLoaded = lpTag.events.hasFired('SCRAPER','ZONES_LOADED')
+            var possibleZones = onesLoaded && zonesLoaded[0] && zonesLoaded[0].data
             // is this an embedded button (engagementType 5) and an HTML engagement (renderingType 1)
             if (data.engagementType === 5 && data.renderingType === 1) {
                 // find the zone
-                var thisZone = possibleZones.find(function (possibleZone) { return possibleZone.id === data.zoneId})
+                var thisZone;
+                for (var i=0; i < possibleZones.length; i++) {
+                    if (possibleZones[i].id === data.zoneId) { thisZone = possibleZones[i]}
+                }
                 // remove role and tabindex from relevant div
                 if (thisZone) {
                     const selectedDiv = document.getElementById(thisZone.name);
@@ -33,4 +29,3 @@ lpTag.external.accessibilityFix = {
 }
 
 lpTag.events.bind('LP_OFFERS','OFFER_IMPRESSION', lpTag.external.accessibilityFix.engagementRenderedHandler);
-console.log('accessibilityfix loaded');
