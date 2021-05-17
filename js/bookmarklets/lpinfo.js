b8c43d3413e56b55c4734594a3a8e7fa2571297f
Mark Manguno <mmanguno@liveperson.com>
2021-05-17 15:13:06 -0400

    lpinfo try/catch

--- js/bookmarklets/lpinfo.js
+++ js/bookmarklets/lpinfo.js
@@ -1,45 +1,4 @@
 (function () {
-    var allEvents = lpTag.events.hasFired("*","*");
-    var convEvents = lpTag.events.hasFired("lpUnifiedWindow", "conversationInfo");
-    var windowStateEvents = lpTag.events.hasFired("lpUnifiedWindow", "state");
-    var renderEvents = lpTag.events.hasFired("RENDERER_STUB", "AFTER_CREATE_ENGAGEMENT_INSTANCE");
-    var engagementClicks = lpTag.events.hasFired("LP_OFFERS", "OFFER_CLICK");
-    var eventMap = allEvents.map(function (e) {return e.appName + e.eventName})
-    var startPageIndex = eventMap.lastIndexOf("lp_monitoringSDKSP_SENT")
-    var eventsAfterSP = allEvents.slice(startPageIndex)
-    var engagementsAfterSP = eventsAfterSP.filter(function (e) {
-        return e.appName === "RENDERER_STUB" && e.eventName === "AFTER_CREATE_ENGAGEMENT_INSTANCE"
-    })
-    var lastDisplayedEngagements = engagementsAfterSP.map(_extractEngDetails) || [];
-    var displayedEngagements = renderEvents.map(_extractEngDetails) || [];
-    var latestEngagementClick = _getLatest(engagementClicks) || {};
-    var clickedEngagementRender = _findRenderEvent(renderEvents, latestEngagementClick.engagementId) || {};
-    var clickedEngagement = _extractEngDetails(clickedEngagementRender)
-    var lpVidCookie = document.cookie.split("; ").find(function(row) {
-        return row.startsWith("LPVID");
-    });
-    var lpSidCookie = document.cookie.split("; ").find(function(row) {
-        return row.startsWith("LPSID-".concat(lpTag.site));
-    });
-    var lpVid = lpVidCookie ? lpVidCookie.split("=")[1] : undefined;
-    var lpSid = lpSidCookie ? lpSidCookie.split("=")[1] : undefined;
-    var ceVid = _getLatest(convEvents, "visitorId");
-    var pid = lpVid !== ceVid ? ceVid : undefined;
-    console.log({
-        clickedEngagement: clickedEngagement,
-        latestSkillId: _getLatest(convEvents, "skill"),
-        latestAgentId: _getLatest(convEvents, "agentId"),
-        latestConvId: _getLatest(convEvents, "conversationId"),
-        latestAgentName: _getLatest(convEvents, "agentName"),
-        latestWindowState: _getLatest(windowStateEvents, "state"),
-        displayedEngagements: displayedEngagements,
-        lastDisplayedEngagements: lastDisplayedEngagements,
-        lpSid: lpSid,
-        lpVid: lpVid,
-        pid: pid,
-        siteId: lpTag.site,
-        sections: lpTag.section
-    });
 
     function _getLatest(array, datum) {
         var event = undefined;
@@ -76,5 +35,52 @@
             }
         }
         return details;
+    }
+
+    try {
+        var allEvents = lpTag.events.hasFired("*","*");
+        var convEvents = lpTag.events.hasFired("lpUnifiedWindow", "conversationInfo");
+        var windowStateEvents = lpTag.events.hasFired("lpUnifiedWindow", "state");
+        var renderEvents = lpTag.events.hasFired("RENDERER_STUB", "AFTER_CREATE_ENGAGEMENT_INSTANCE");
+        var engagementClicks = lpTag.events.hasFired("LP_OFFERS", "OFFER_CLICK");
+        var eventMap = allEvents.map(function (e) {return e.appName + e.eventName})
+        var startPageIndex = eventMap.lastIndexOf("lp_monitoringSDKSP_SENT")
+        var eventsAfterSP = allEvents.slice(startPageIndex)
+        var engagementsAfterSP = eventsAfterSP.filter(function (e) {
+            return e.appName === "RENDERER_STUB" && e.eventName === "AFTER_CREATE_ENGAGEMENT_INSTANCE"
+        })
+        var lastDisplayedEngagements = engagementsAfterSP.map(_extractEngDetails) || [];
+        var displayedEngagements = renderEvents.map(_extractEngDetails) || [];
+        var latestEngagementClick = _getLatest(engagementClicks) || {};
+        var clickedEngagementRender = _findRenderEvent(renderEvents, latestEngagementClick.engagementId) || {};
+        var clickedEngagement = _extractEngDetails(clickedEngagementRender)
+        var lpVidCookie = document.cookie.split("; ").find(function(row) {
+            return row.startsWith("LPVID");
+        });
+        var lpSidCookie = document.cookie.split("; ").find(function(row) {
+            return row.startsWith("LPSID-".concat(lpTag.site));
+        });
+        var lpVid = lpVidCookie ? lpVidCookie.split("=")[1] : undefined;
+        var lpSid = lpSidCookie ? lpSidCookie.split("=")[1] : undefined;
+        var ceVid = _getLatest(convEvents, "visitorId");
+        var pid = lpVid !== ceVid ? ceVid : undefined;
+        console.log({
+            clickedEngagement: clickedEngagement,
+            latestSkillId: _getLatest(convEvents, "skill"),
+            latestAgentId: _getLatest(convEvents, "agentId"),
+            latestConvId: _getLatest(convEvents, "conversationId"),
+            latestAgentName: _getLatest(convEvents, "agentName"),
+            latestWindowState: _getLatest(windowStateEvents, "state"),
+            displayedEngagements: displayedEngagements,
+            lastDisplayedEngagements: lastDisplayedEngagements,
+            lpSid: lpSid,
+            lpVid: lpVid,
+            pid: pid,
+            siteId: lpTag.site,
+            sections: lpTag.section
+        });
+
+    } catch (e) {
+        console.error(e)
     }
 })()
